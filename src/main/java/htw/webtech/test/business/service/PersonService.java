6dd546f4ce3e75428a8e4f0a7ab8edfd07aefd1d
foodismood <Sean.Genkel@Student.HTW-Berlin.de>
2025-12-01 17:29:09 +0100

    gradle update

--- src/main/java/htw/webtech/test/business/service/PersonService.java
+++ src/main/java/htw/webtech/test/business/service/PersonService.java
@@ -1,10 +1,12 @@
 package htw.webtech.test.business.service;
 
 import htw.webtech.test.rest.model.PersonDTO;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
+@Service
 public class PersonService{
     public List<PersonDTO> getAllPersons(){
         PersonDTO personDTO = new PersonDTO("Sean", "Genkel");
